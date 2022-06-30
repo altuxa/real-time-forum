9b8ab7cbed92e02deae9878e463f1f77406cf9b3
Altynbek <altuxadark98@gmail.com>
2022-06-30 17:18:28 +0600

    test

--- internal/handler/http/handler.go
+++ internal/handler/http/handler.go
@@ -30,10 +30,19 @@ func (h *Handler) NewServer() {
 	r := new(router.Router)
 	r.Route("GET", "/", h.HomePage)
 	r.Route("POST", "/users/sign-up", h.SignUp)
+	r.Route("POST", "/users/sign-in", h.SignIn)
 	r.Route("POST", "/posts", h.CreatePost)
 	r.Route("POST", "/posts/comments", h.CreateComment)
+	// r.Route("GET", "/test", func(rw http.ResponseWriter, r *http.Request) {
+	// 	a := rw.Header().Get("Aboba")
+	// 	b := r.Header.Get("Aboba")
+	// 	rw.Header().Set("Dota", "ursa")
+	// 	r.Header.Set("Dota", "lina")
+	// 	rw.Write([]byte("jojo" + a + b))
+	// })
 	log.Println("Server started")
-	if err := http.ListenAndServe(":8080", r); err != nil {
+	handler := Auth(r)
+	if err := http.ListenAndServe(":8080", handler); err != nil {
 		log.Fatalln(err)
 	}
 }
